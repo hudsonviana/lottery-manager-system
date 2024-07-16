fc228742f7451fbadc5d76676b8cc529fa7cf0ad
hudsonviana <hudson1206@gmail.com>
2024-07-15 22:32:55 -0300

    Middleware de autenticação completo

---
+++ backend/src/middlewares/auth.js
@@ -0,0 +1,20 @@
+import 'dotenv/config';
+import jwt from 'jsonwebtoken';
+
+export const authentication = async (req, res, next) => {
+  // const token = req.header('Authorization');
+
+  if (!req.headers.authorization) {
+    return res.status(401).json({ error: 'Acesso negado' });
+  }
+
+  const token = req.headers.authorization.split(' ')[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.SECRET_KEY);
+    req.id = decoded.id;
+    next();
+  } catch (error) {
+    res.status(401).json({ error: 'Token inválido' });
+  }
+};
--- backend/src/routes/api.js
+++ backend/src/routes/api.js
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { authentication } from '../middlewares/auth.js';
 import * as authController from '../controllers/authController.js';
 import * as userController from '../controllers/userController.js';
 
@@ -7,7 +8,7 @@ const router = Router();
 router.post('/auth/register', authController.register);
 router.post('/auth/login', authController.login);
 
-router.get('/users', userController.getAll);
-router.get('/users/:id', userController.getUser);
+router.get('/users', authentication, userController.getAll);
+router.get('/users/:id', authentication, userController.getUser);
 
 export default router;
