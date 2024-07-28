5e22d2cdbcc8a764113625c9722a2a7f2ca7fd20
hudsonviana <hudson1206@gmail.com>
2024-07-27 22:43:54 -0300

    Instalar Shadcn

--- src/Auth/Login.jsx
+++ src/Auth/Login.jsx
@@ -1,12 +1,9 @@
-import React from 'react'
-
 const Login = () => {
   return (
     <div>
       <h1>Acessar o sistema</h1>
-      
     </div>
   );
-}
+};
 
-export default Login
+export default Login;
