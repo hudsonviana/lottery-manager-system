d969f6faa34f7b7e32c1621ee8784cd0ef126e4a
hudsonviana <hudson1206@gmail.com>
2024-11-13 23:34:52 -0300

    Implementação de ícone em useToastAlert

--- src/hooks/useToastAlert.js
+++ src/hooks/useToastAlert.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import { useToast } from './use-toast';
+import { FiCheckCircle, FiAlertTriangle, FiXCircle, FiInfo } from 'react-icons/fi';
 
 const useToastAlert = () => {
   const { toast, dismiss } = useToast();
 
-  const getColor = (type) => {
-    const typeColor = {
-      primary: 'blue',
-      warning: 'yellow',
-      danger: 'red',
-      success: 'green',
+  const getColorIcon = (type) => {
+    const types = {
+      primary: ['blue', React.createElement(FiInfo, { className: 'mr-2' })],
+      success: ['green', React.createElement(FiCheckCircle, { className: 'mr-2' })],
+      warning: ['yellow', React.createElement(FiAlertTriangle, { className: 'mr-2' })],
+      danger: ['red', React.createElement(FiXCircle, { className: 'mr-2' })],
     };
-    return typeColor[type] || 'blue';
+    return types[type] || ['blue', React.createElement(FiInfo, { className: 'mr-2' })];
   };
 
   const toastAlert = ({ type, title, message }) => {
-    const color = getColor(type);
+    const [color, icon] = getColorIcon(type);
 
     const className = [
       `bg-${color}-200`,
@@ -27,7 +28,16 @@ const useToastAlert = () => {
       ? message.map((msg, index) => React.createElement('p', { key: index }, `• ${msg}`))
       : message;
 
-    return toast({ className, title, description });
+    return toast({
+      className,
+      title: React.createElement('div', { className: 'flex items-center' }, [
+        [
+          React.cloneElement(icon, { key: 'icon' }),
+          React.createElement('span', { key: 'title' }, title),
+        ],
+      ]),
+      description,
+    });
   };
   return { toastAlert, dismiss };
 };
